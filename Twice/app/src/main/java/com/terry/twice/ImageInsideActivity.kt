2902531f26ee4f40c82f9ce9d41d5635d8d2d8d3
junho100 <bemodesty306@naver.com>
2023-06-25 16:14:48 +0900

    section 2

---
+++ Twice/app/src/main/java/com/terry/twice/ImageInsideActivity.kt
@@ -0,0 +1,26 @@
+package com.terry.twice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class ImageInsideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_inside)
+
+        val getData = intent.getStringExtra("data")
+
+        val memberImage = findViewById<ImageView>(R.id.memberImageArea)
+
+        if (getData == "1") {
+            memberImage.setImageResource(R.drawable.member_1)
+        }
+        if (getData == "2") {
+            memberImage.setImageResource(R.drawable.member_2)
+        }
+        if (getData == "3") {
+            memberImage.setImageResource(R.drawable.member_3)
+        }
+    }
+}
--- Twice/app/src/main/java/com/terry/twice/MainActivity.kt
+++ Twice/app/src/main/java/com/terry/twice/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.terry.twice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1 = findViewById<ImageView>(R.id.member1)
+        val btn2 = findViewById<ImageView>(R.id.member2)
+        val btn3 = findViewById<ImageView>(R.id.member3)
+        btn1.setOnClickListener{
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "1")
+            startActivity(intent)
+        }
+
+        btn2.setOnClickListener{
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "2")
+            startActivity(intent)
+        }
+
+        btn3.setOnClickListener{
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "3")
+            startActivity(intent)
+        }
     }
 }
